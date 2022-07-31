96071b82478ef282780d14e1bff8e976a44a85c5
Pixaler <gnidko02@gmail.com>
2022-07-31 12:52:28 +0300

    v0.8

--- Changelog.md
+++ Changelog.md
@@ -23,3 +23,20 @@
 ## Changes
 1. Update YT apk to 17.28.34
 2. Exclude premium-heading and sponsorblock patches. Include swipe-controls
+
+# v0.7
+## Changes
+1. Update YT apk to 17.29.34
+2. Script rewrite. Now you input ADB device number at first time and choose what do you want to build
+3. Excluded custom-branding patches
+4. Add message to close jar builiding window
+
+# v0.7.1
+## Changes
+1. Now code looks like much better
+2. Message that appear more clearer
+
+# v0.8
+## Changes
+1. Return options to not download revanced resource
+2. Add downloading YT or YT Music. Its increase speed of publishing releases much faster
--- _build.cmd
+++ _build.cmd
@@ -1,222 +1,259 @@
 @echo off
-:: Check apk file for script
-:checkytapk
-if not exist youtube1.apk (
-    echo "Please download YouTube apk. Then rename YouTube to youtube1.apk"
-    echo "Check avaliable version that support patch: https://github.com/revanced/revanced-patches"
-    pause
-    goto:checkapk
+:: Download or not apk files
+cls
+:downloadyoutube
+set /P downloadyt=Download YT apk (y/n): 
+
+if %downloadyt%==y (
+    call :downloadapps
+    goto :downloadrevancedoption
 )
-checkytmapk
-if not exist youtube2.apk (
-    echo "Please downloadYouTube Music apk. Then rename YouTube Music to youtube2.apk"
-    echo "Check avaliable version that support patch: https://github.com/revanced/revanced-patches"
-    pause
-    goto:checkytmapk
+if %downloadyt%==n (
+    goto :downloadrevancedoption
+) else (
+    echo Type y - yes or n - no
+    echo .
+    echo .
+    goto :downloadyoutube
 )
 
-:: Download or not download resources
-:choice2
-set /p answer=Download files or update files?(y/n):  
-if %answer%==y (
-:: Delete old files
-    echo ---Delete previous files-- 
-    del /q /s revanced*.dex
-    del /q /s revanced*.jar
-    del /q /s app-release-unsigned.apk
-:: Download ReVanced Integrations
-    echo ---Download ReVanced Integrations---
-    powershell.exe ./ri.ps1
-    timeout 5 > NUL /nobreak
-:: Download ReVanced Patches
-    echo ---Download ReVanced CLI---
-    powershell.exe ./rcli.ps1
-    timeout 5 > NUL /nobreak
-:: Download ReVanced CLI
-    echo ---Download ReVanced Patches---
-    powershell.exe ./rp.ps1
-    del /q /s revanced*.dex
-    timeout 5 > NUL /nobreak
-:: Rename files
-    echo ---Rename files---
-    ren revanced-cli*.jar revanced-cli-all.jar
-    ren revanced-patches*.jar revanced-patches.jar
-    goto:adb
-) 
-if %answer%==n (
-    goto:adb
+:: Download or not revanced files
+:downloadrevancedoption
+cls
+echo Download revanced files? (y/n)
+set /P downloadrevanced=You options is: 
+
+if %downloadrevanced%==y (
+    call :downloadrevanced
+)
+if %downloadrevanced%==n (
+    goto :adb
 ) else (
-    goto:choice2
+    echo Type y - yes or n - no
+    echo .
+    echo .
+    goto :downloadrevancedoption
 )
 
 :: Check ADB connection
 :adb
-echo ---Check ADB connection---
-adb devices
-echo Garant root to shell on your phone if it's stuck!
-adb shell su -c exit
+    cls
+    echo ---Check ADB connection---
+    echo .
+    echo .
+    adb devices
+    echo Give ADB access to this PC, if its needed
+    echo .
+    timeout 5 > NUL /nobreak
+    echo .
+    echo Garant root to shell on your phone, if it's stuck!
+    adb shell su -c exit
+    echo .
+    timeout 5 > NUL /nobreak
+    echo .
 
 :: Read adb number
-set /p adb=< %cd%\adb.txt
-echo Your devices: %adb%
-set /p ans2=Is it right?(y/n): 
-if %ans2%==n (
-    goto:changeadb
-) 
-if %ans2%==y (
-    goto:choice3
+if exist adb.txt (
+    call :mesadb
 ) else (
-    goto:adb
+    call :changeadb
 )
+
+:: What do you want to build
+:build
+    echo 1. YouTube ReVanced Root
+    echo 2. YouTube Music Revanced Root
+    echo 3. All Root  
+    echo 4. YouTube ReVanced Non-Root
+    echo 5. YouTube Music ReVanced Non-Root
+    echo 6. All Non-Root
+    set /P option=You options is: 
+    cls
+
+call :menu-n-%option% 2>nul ||(
+    echo Type number from 1 - 6
+    echo .
+    echo .
+    goto :build
+)
+
+:menu-n-1 
+    call :ytroot
+    goto :scriptend
+
+:menu-n-2 
+    call :ytmroot
+    goto :scriptend
+
+:menu-n-3 
+    call :ytroot
+    call :ytmroot
+    goto :scriptend
+
+:menu-n-4
+    call :ytnonroot
+    goto :scriptend
+
+:menu-n-5
+    call :ytmnonroot
+    goto :scriptend
+
+:menu-n-6
+    call :ytnonroot
+    call :ytmnonroot
+    goto :scriptend
+
+:downloadapps
+    echo ---Download YouTube 17.29.34---
+    del /q /s youtube1.apk
+    start "" C:/BRWF/wget/bin/wget.exe --content-disposition https://cdn-118.anonfiles.com/Z1x1451ay9/0ff204f5-1659259027/youtube1.apk
+    pause
+    echo ---Download YouTube Music 5.14.53---
+    del /q /s youtube2.apk
+    start "" C:/BRWF/wget/bin/wget.exe --content-disposition https://cdn-133.anonfiles.com/11r44b1bye/da3f2112-1659258556/youtube2.apk
+    pause
+    goto :downloadrevancedoption
+
+:downloadrevanced
+:: Delete old files
+    echo ---Delete previous files-- 
+    echo .
+    echo .
+    del /q /s revanced*.dex
+    del /q /s revanced*.jar
+    del /q /s app-release-unsigned.apk
+
+:: Download ReVanced Integrations
+    echo ---Download ReVanced Integrations---
+    echo .
+    echo .
+    powershell.exe ./ri.ps1
+    timeout 5 > NUL /nobreak
+
+:: Download ReVanced Patches
+    echo ---Download ReVanced CLI---
+    echo .
+    echo .
+    powershell.exe ./rcli.ps1
+    timeout 5 > NUL /nobreak
+
+:: Download ReVanced CLI
+    echo ---Download ReVanced Patches---
+    echo .
+    echo .
+    powershell.exe ./rp.ps1
+    del /q /s revanced*.dex
+    timeout 5 > NUL /nobreak
+
+:: Rename files
+    echo ---Rename files---
+    echo .
+    echo .
+    ren revanced-cli*.jar revanced-cli-all.jar
+    ren revanced-patches*.jar revanced-patches.jar
+    goto :adb
 
 :: Input your ADB number devices
 :changeadb
-set /p adbw=Type or copy your adb number (only number): 
-echo %adbw%
-echo %adbw% > adb.txt
-goto:adb
+    set /p adbw=Type or copy your adb number (only number): 
+    echo .
+    echo .
+    echo %adbw%
+    echo %adbw% > adb.txt
+    goto :adb
 
-:: Propose to uninstall app
-:choice3
-echo What do you what to uninstall?
-echo 1. Uninstall YouTube
-echo 2. Uninstall YouTube Music
-echo 3. Uninstall all
-echo 4. None
-set /p unin=Your choice: 
-if %unin%==1 (
-    echo "---Uninstalling YouTube---"
+:: Show your ADB number
+:mesadb
+    set /p adb=< %cd%\adb.txt
+    echo Your devices: %adb%
+    echo If its diffrent delete adb.txt and run script again or copy your ADB device number and replace it instead of number in adb.txt 
+    echo .
+    echo .
+    echo .
+    echo .
+    goto :build
+
+:ytroot
+    echo ---Uninstalling YouTube---
+    echo .
+    echo .
     adb -s %adb% uninstall com.google.android.youtube
-    goto:choice4
-)
-if %unin%==2 (
-    echo "---Uninnstalling YouTube Music---"
-    adb -s %adb% uninstall com.google.android.apps.youtube.music
-    goto:choice4
-)
-if %unin%==3 (
-    echo "---Uninstalling All---"
-    adb -s %adb% uninstall com.google.android.youtube
-    adb -s %adb% uninstall com.google.android.apps.youtube.music
-    goto:choice4
-) 
-if %unin%==4 (
-    goto:choice4
-) else (
-    goto:choice3
-)
-
-:: Propose to install YT
-:choice4
-echo What do you what to install?
-echo 1. Install YouTube
-echo 2. Install YouTube Music
-echo 3. Install Vanced Microg
-echo 4. Install all for Root
-echo 5. Install all for Non-Root
-echo 6. None
-set /p insyt=Your choice: 
-if %insyt%==1 (
-    echo "---Installing YouTube---"
+    echo ---Installing YouTube---
+    echo .
+    echo .
     adb -s %adb% install youtube1.apk
-    goto:choice5
-)
-if %insyt%==2 (
-    echo "---Installing YouTube Music---"
-    adb -s %adb% install youtube2.apk
-    goto:choice5
-)
-if %insyt%==3 (
-    echo "---Installing Vanced Microg---"
-    adb -s %adb% install microg.apk
-    goto:choice5
-)
-if %insyt%==4 (
-    echo "---Installing YouTube---"
-    echo ""
-    adb -s %adb% install youtube1.apk
-    echo "---Installing YouTube Music---"
-    adb -s %adb% install youtube2.apk
-    goto:choice5
-)
-if %insyt%==5 (
-    echo "---Installing Vanced Microg---"
-    echo ""
-    adb -s %adb% install microg.apk
-    echo "---Installing YouTube---"
-    echo ""
-    adb -s %adb% install youtube1.apk
-    echo "---Installing YouTube Music---"
-    adb -s %adb% install youtube2.apk
-    goto:choice5
-)
-if %insyt%==6 (
-    goto:choice5
-) else (
-    goto:choice4
-)
-:: What do you want to build
-:choice5
-echo 1. YouTube ReVanced Root
-echo 2. YouTube Music Revanced Root
-echo 3. All Root  
-echo 4. YouTube ReVanced Non-Root
-echo 5. YouTube Music ReVanced Non-Root
-echo 6. All Non-Root
-set /P option=You options is: 
-
-if %option%==1 (
-    echo "---Build Youtube ReVanced Root---"
-    echo ""
-    start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube1.apk -c -d %adb% -o revanced_youtube.apk -b revanced-patches.jar -m app-release-unsigned.apk -e microg-support -e premium-heading -e sponsorblock -i swipe-controls --mount
+    echo ---Build Youtube ReVanced Root---
+    echo .
+    echo .
+    echo When building is over, close jar window and press any key
+    start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube1.apk -c -d %adb% -o revanced_youtube.apk -b revanced-patches.jar -m app-release-unsigned.apk -e microg-support -e premium-heading -e custom-branding -e sponsorblock -i swipe-controls --mount
     pause
-    goto:eof
-)
-if %option%==2 (
-    echo "---Build Youtube Music Revanced Root---"
-    echo ""
+    goto :eof
+
+:ytmroot
+    echo ---Uninstalling YouTube Music---
+    echo .
+    echo .
+    adb -s %adb% uninstall com.google.android.apps.youtube.music
+    echo ---Installing YouTube Music---
+    echo .
+    echo .
+    adb -s %adb% install youtube2.apk
+    echo ---Build Youtube Music Revanced Root---
+    echo .
+    echo .
+    echo When building is over, close jar window and press any key
     start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube2.apk -c -d %adb% -o revanced_music.apk -b revanced-patches.jar -m app-release-unsigned.apk -e microg-support --mount
     pause
-    goto:eof
-)
-if %option%==3 (
-    echo "---Build Youtube ReVanced Root---"
-    echo ""
-    start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube1.apk -c -d %adb% -o revanced_youtube.apk -b revanced-patches.jar -m app-release-unsigned.apk -e microg-support -e premium-heading -e sponsorblock -i swipe-controls --mount
+    goto :eof
+
+:ytnonroot
+    echo ---Uninstalling YouTube---
+    echo .
+    echo .
+    adb -s %adb% uninstall com.google.android.youtube
+    echo ---Installing Vanced Microg---
+    echo .
+    echo .
+    adb -s %adb% install microg.apk
+    echo ---Installing YouTube---
+    echo .
+    echo .
+    adb -s %adb% install youtube1.apk
+    echo ---Build Youtube ReVanced Non-Root---
+    echo .
+    echo .
+    echo If display any warning that prevent instaling app through ADB - apply it!
+    echo .
+    echo .
+    echo When building is over, close jar window and press any key
+    start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube1.apk -c -d %adb% -o revanced_youtube.apk -b revanced-patches.jar -m app-release-unsigned.apk -e premium-heading -e custom-branding -e sponsorblock -i swipe-controls
     pause
-    echo "---Build Youtube Music Revanced Root---"
-    start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube2.apk -c -d %adb% -o revanced_music.apk -b revanced-patches.jar -m app-release-unsigned.apk -e microg-support --mount
-    pause
-    goto:eof
-) 
-if %option%==4 (
-    echo "---Build Youtube ReVanced Non-Root---"
-    echo ""
-    echo "If display any warning that prevent instaling app through ADB - apply it!"
-    start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube1.apk -c -d %adb% -o revanced_youtube.apk -b revanced-patches.jar -m app-release-unsigned.apk -e premium-heading -e sponsorblock -i swipe-controls
-    pause
-    goto:eof
-)
-if %option%==5 (
-    echo "---Build Youtube Music Revanced Non-Root---"
-    echo ""
-    echo "If display any warning that prevent instaling app through ADB - apply it!"
+    goto :eof
+
+:ytmnonroot
+    echo ---Uninstalling YouTube Music---
+    echo .
+    echo .
+    adb -s %adb% uninstall com.google.android.apps.youtube.music
+    echo ---Installing Vanced Microg---
+    echo .
+    echo .
+    adb -s %adb% install microg.apk
+    echo ---Installing YouTube Music---
+    echo .
+    echo .
+    adb -s %adb% install youtube2.apk
+    echo ---Build Youtube Music Revanced Non-Root---
+    echo .
+    echo .
+    echo If display any warning that prevent instaling app through ADB - apply it!
+    echo .
+    echo .
+    echo When building is over, close jar window and press any key
     start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube2.apk -c -d %adb% -o revanced_music.apk -b revanced-patches.jar -m app-release-unsigned.apk
     pause
-    goto:eof
-)
-if %option%==6 (
-    echo "---Build Youtube ReVanced Non-Root---"
-    echo ""
-    echo "If display any warning that prevent instaling app through ADB - apply it!"
-    start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube1.apk -c -d %adb% -o revanced_youtube.apk -b revanced-patches.jar -m app-release-unsigned.apk -e premium-heading -e sponsorblock -i swipe-controls
-    pause
-    echo "---Build Youtube Music Revanced Non-Root---"
-    echo ""
-    echo "If display any warning that prevent instaling app through ADB - apply it!"
-    start "" C:/BRWF/zulu17/bin/java -jar revanced-cli-all.jar -a youtube2.apk -c -d %adb% -o revanced_music.apk -b revanced-patches.jar -m app-release-unsigned.apk
-    pause
-    goto:eof
-) else (
-    goto:choice5
-)  
+    goto :eof
+
+:scriptend
+    exit
